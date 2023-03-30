c98678c34326f34889752ed710b98bc2fd754dfd
RiuHibryd <3043251630114@ingenieria.usac.edu.gt>
2023-03-30 17:21:31 -0600

    update

--- main.py
+++ main.py
@@ -8,61 +8,61 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        # Initialization button
+        # Boton de inicializacion
         self.init_button = tk.Button(self)
-        self.init_button["text"] = "Initialization"
+        self.init_button["text"] = "Inicializar"
         self.init_button.pack(side="top")
 
-        # Load input file button
+        # Boton de carga de archivo de entrada
         self.load_input_button = tk.Button(self)
-        self.load_input_button["text"] = "Load input XML file"
+        self.load_input_button["text"] = "Cargar archivo de entrada"
         self.load_input_button.pack(side="top")
 
-        # Generate output file button
+        # Generar archivo de salida
         self.generate_output_button = tk.Button(self)
-        self.generate_output_button["text"] = "Generate output XML file"
+        self.generate_output_button["text"] = "Generar archivo de salida"
         self.generate_output_button.pack(side="top")
 
-        # Chemical element management frame
-        self.chem_frame = tk.LabelFrame(self, text="Chemical element management")
+        # Manejo de elementos quimicos frame
+        self.chem_frame = tk.LabelFrame(self, text="Manejo de elementos quimicos")
         self.chem_frame.pack(side="left")
 
-        # View chemical elements button
+        # Mirar elementos quimicos 
         self.view_elements_button = tk.Button(self.chem_frame)
-        self.view_elements_button["text"] = "View chemical elements sorted by atomic number"
+        self.view_elements_button["text"] = "Ver elementos quimicos por numero atomico"
         self.view_elements_button.pack(side="top")
 
-        # Add new chemical element button
+        # Agregar elemento quimico
         self.add_element_button = tk.Button(self.chem_frame)
-        self.add_element_button["text"] = "Add new chemical element"
+        self.add_element_button["text"] = "Agregar elemento quimico"
         self.add_element_button.pack(side="top")
 
-        # Compound management frame
-        self.comp_frame = tk.LabelFrame(self, text="Compound management")
+        # Manejo de compuestos frame
+        self.comp_frame = tk.LabelFrame(self, text="Manejo de compuestos")
         self.comp_frame.pack(side="left")
 
-        # View compounds button
+        # Ver compuestos y sus formulas
         self.view_compounds_button = tk.Button(self.comp_frame)
-        self.view_compounds_button["text"] = "View compounds and their formulas"
+        self.view_compounds_button["text"] = "Compuestos y sus formulas"
         self.view_compounds_button.pack(side="top")
 
-        # Analyze compound button
+        # Analizar compuesto
         self.analyze_compound_button = tk.Button(self.comp_frame)
-        self.analyze_compound_button["text"] = "Analyze compound"
+        self.analyze_compound_button["text"] = "Analizar compuesto"
         self.analyze_compound_button.pack(side="top")
 
-        # Machine management frame
-        self.mach_frame = tk.LabelFrame(self, text="Machine management")
+        # Manejo de maquinas frame
+        self.mach_frame = tk.LabelFrame(self, text="Manejo de maquinas")
         self.mach_frame.pack(side="left")
 
-        # View machines button
+        # Ver maquinas
         self.view_machines_button = tk.Button(self.mach_frame)
-        self.view_machines_button["text"] = "View machines"
+        self.view_machines_button["text"] = "Ver maquinas"
         self.view_machines_button.pack(side="top")
 
-        # Help button
+        # Ayuda
         self.help_button = tk.Button(self)
-        self.help_button["text"] = "Help"
+        self.help_button["text"] = "Ayudame"
         self.help_button.pack(side="bottom")
 
 root = tk.Tk()
