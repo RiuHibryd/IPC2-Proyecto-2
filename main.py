6f8442d6dfba22895935fd959f6c244d80a9c76a
RiuHibryd <3043251630114@ingenieria.usac.edu.gt>
2023-04-02 10:39:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import tkinter as tk
-from TDA.Quimicos import *
+from TDA.Quimicos import Quimicos, load_xml_file
+from tkinter import ttk
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
         self.create_widgets()
+
+
 
     def create_widgets(self):
         # Boton de inicializacion
